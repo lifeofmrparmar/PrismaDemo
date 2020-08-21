4264d9b1a2a936e4e561a53d68f99ff40400423a
Mayur Parmar <mrparmar2997@gmail.com>
2020-08-21 18:03:45 +0530

    - Initial Commit

--- PrismaDemo/ViewController.swift
+++ PrismaDemo/ViewController.swift
@@ -7,14 +7,123 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var imgHeight: NSLayoutConstraint!
+    @IBOutlet weak var buttonChooseImg: UIButton!
+    @IBOutlet weak var buttonApplyFilter: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    
+    //MARK:- Other Methods
+    func setupUI() {
+        imagePicker.delegate = self
+        photo.layer.masksToBounds = true
+        
+        self.imgHeight.constant = self.view.bounds.width
+        buttonChooseImg.addTarget(self, action: #selector(didTapButtonChooseImg), for: .touchUpInside)
+        buttonApplyFilter.addTarget(self, action: #selector(didTapButtonApplyFilter), for: .touchUpInside)
     }
-
-
+    
+    func pixelBuffer(from image: UIImage) -> CVPixelBuffer? {
+        // 1
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 256, height: 256), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 256, height: 256))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // 2
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, 256, 256, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        // 3
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        // 4
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: 256, height: 256, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        // 5
+        context?.translateBy(x: 0, y: 256)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        // 6
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: 256, height: 256))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
 }
 
+//MARK:- ViewController LifeCyele
+extension ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.viewDidAppear(true)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+    }
+}
+
+
+//MARK:- IBAction
+extension ViewController {
+    @objc func didTapButtonChooseImg() {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @objc func didTapButtonApplyFilter() { 
+        let model = StarryStyle()
+        
+        let styleArray = try? MLMultiArray(shape: [1] as [NSNumber], dataType: .double)
+        styleArray?[0] = 1.0
+        
+        if let image = pixelBuffer(from: photo.image!) {
+            do {
+                let predictionOutput = try model.prediction(image: image, index: styleArray!)
+                
+                let ciImage = CIImage(cvPixelBuffer: predictionOutput.stylizedImage)
+                let tempContext = CIContext(options: nil)
+                let tempImage = tempContext.createCGImage(ciImage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(predictionOutput.stylizedImage), height: CVPixelBufferGetHeight(predictionOutput.stylizedImage)))
+                photo.image = UIImage(cgImage: tempImage!)
+            } catch let error as NSError {
+                print("CoreML Model Error: \(error)")
+            }
+        }
+    }
+}
+
+
+//MARK:- UIImagePickerControllerDelegate
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photo.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+     
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
